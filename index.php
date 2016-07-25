624034f3efca1aa395a8b6bbdc7f990b9bf2f859
Attogram <barberi.office@gmail.com>
2016-07-26 01:15:18 +0200

    autoload psr4, upd

--- index.php
+++ index.php
@@ -6,21 +6,20 @@ namespace Attogram\htfaker;
 define('', '0.0.1');
 
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/htfaker/htfaker.php';
 
 $htfaker = new htfaker();
-
 $testFile = $htfaker->getHtaccessFile('tests/.htaccess');
-
 $errorDocument = $testFile->search('ErrorDocument');
 $options = $testFile->search('Options');
 $modRewrite = $testFile->search('modRewrite');
 
 $cr = "\n";
 
-echo '<pre>htfaker ', htfaker::HTFAKER_VERSION, $cr;
-echo $cr, '(string)$testFile:', $cr, (string)$testFile;
-echo $cr, '$testFile:', $cr, print_r($testFile, true);
-echo $cr, '$errorDocument: ', print_r($errorDocument, true);
-echo $cr, '$options: ', print_r($options, true);
-echo $cr, 'modRewrite: ', print_r($modRewrite, true);
+echo '<pre>htfaker ', htfaker::HTFAKER_VERSION,
+    ' - ', date('Y-m-d h:i:s'), ' - ',
+    ' https://github.com/attogram/htfaker', $cr,
+    $cr, '(string)$testFile:', $cr, (string)$testFile,
+    $cr, '$testFile:', $cr, print_r($testFile, true),
+    $cr, '$errorDocument: ', print_r($errorDocument, true),
+    $cr, '$options: ', print_r($options, true),
+    $cr, 'modRewrite: ', print_r($modRewrite, true);
